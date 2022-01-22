d3fab8ea95c6ec84ddec05d4dcf23aba62177c84
murshidazher <murshid@different.com.au>
2022-01-22 20:16:17 +0530

    :bug: fix: add missing putmapping controller

--- src/main/java/com/ase/budgetase/controller/BudgetController.java
+++ src/main/java/com/ase/budgetase/controller/BudgetController.java
@@ -6,19 +6,7 @@ import com.ase.budgetase.service.UtilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-=======
 import org.springframework.web.bind.annotation.*;
->>>>>>> a68b9ae (:sparkles: feat: import missing modules)
 
 
 @RestController
