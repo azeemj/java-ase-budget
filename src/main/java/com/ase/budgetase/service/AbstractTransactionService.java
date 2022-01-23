cf8bb9b42500e3d18260bfe079f9b11999649f9a
azemj <azeem@ayra.com.au>
2022-01-24 03:42:57 +0530

    currency

--- src/main/java/com/ase/budgetase/service/AbstractTransactionService.java
+++ src/main/java/com/ase/budgetase/service/AbstractTransactionService.java
@@ -1,9 +1,11 @@
 package com.ase.budgetase.service;
 
 import com.ase.budgetase.entity.Transaction;
+import com.ase.budgetase.logic.CurrencyFactory;
 import com.ase.budgetase.repo.TransactionRepository;
 
 public abstract class AbstractTransactionService {
+	private CurrencyFactory currency;
 	
 	public abstract Transaction saveTrans(Transaction obj, TransactionRepository trans_repo);
 
